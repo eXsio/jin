69c9589559f64fdde2eaec7f21e662fce892adbd
eXsio <slawomir.dymitrow@escsa.pl>
2014-12-03 14:08:16 +0100

    upd tests

--- jin/src/test/java/pl/exsio/jin/file/loader/YamlTranslationFileLoaderImplTest.java
+++ jin/src/test/java/pl/exsio/jin/file/loader/YamlTranslationFileLoaderImplTest.java
@@ -43,12 +43,13 @@ public class YamlTranslationFileLoaderImplTest extends JinBaseTest {
         YamlTranslationFileLoaderImpl instance = new YamlTranslationFileLoaderImpl();
         instance.setLocator(this.getLocator());
         Map<String, String> result = instance.loadFile(TEST_TRANSLATION_PATH);
-        assertTrue(result.size() == 5);
+        assertTrue(result.size() == 6);
         assertEquals(result.get("test.string1.trans"), "String 1 translation");
         assertEquals(result.get("test.string2.trans"), "String 2 translation");
         assertEquals(result.get("test.compound.str2"), "Compound String 2 translation");
         assertEquals(result.get("test.compound2.str1"), "Compound2 String 1 translation");
         assertEquals(result.get("test.compound3.deeper.str1"), "Compound 3 Deeper String 1 translation");
+        assertEquals(result.get("test.mixed.compound.str1"), "Mixed Compound String 1 translation");
 
     }
 
